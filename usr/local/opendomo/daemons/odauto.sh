fa0819af876320e1d8e43b30a2abea201ecd0790
opalenzuela <opalenzuela@gmail.com>
2014-07-04 09:41:05 +0200

    escaping strings

--- usr/local/opendomo/daemons/odauto.sh
+++ usr/local/opendomo/daemons/odauto.sh
@@ -55,7 +55,7 @@ process_odcontrol() {
 				echo "" > $CFGDIR/$DEVNAME/$PNAME.info
 				echo $PVAL  > $CTRLDIR/$DEVNAME/$PNAME.value
 				
-				echo -n "{'Name':'$PNAME','Value':'$PVAL','Id':'$DEVNAME-$PNAME'}," >> /var/www/data/$DEVNAME.odauto
+				echo -n "{\"Name\":\"$PNAME\",\"Value\":\"$PVAL\",\"Id\":\"$DEVNAME-$PNAME\"}," >> /var/www/data/$DEVNAME.odauto
 			fi
 		done
 	fi
@@ -90,7 +90,7 @@ do_background() {
 		done
 		sleep 10
 		echo -n "[" > /var/www/data/odauto.json
-		cat /var/www/data/*.odauto | sed 's/\'/\"/' >> /var/www/data/odauto.json
+		cat /var/www/data/*.odauto  >> /var/www/data/odauto.json
 		echo "]" >> /var/www/data/odauto.json
 		# ln -s /var/www/data/odauto.json /var/www/data/odauto.txt
 	done
