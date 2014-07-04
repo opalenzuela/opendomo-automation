d827ba203f94870cf0a14454fcde07dffb59ee03
opalenzuela <opalenzuela@gmail.com>
2014-07-04 09:16:49 +0200

    without jQuery

--- var/www/scripts/listControlPorts.sh.js
+++ var/www/scripts/listControlPorts.sh.js
@@ -3,19 +3,41 @@ jQuery(function($) {
 	updatePorts();
 });
 
+var portdata;
 function updatePorts()
 {
-	$.getJSON( "/data/odauto.json", function( data ) {
-		var items = [];
-		$.each( data, function( key, val ) {
-			items.push( "<li id='" + key + "'>" + val + "</li>" );
-		});
- 
-		$( "<ul/>", {
-			"class": "my-new-list",
-			html: items.join( "" )
-		}).appendTo( "body" );
-	});
+	var url= "/data/odauto.json";
+	portdata = loadJSON(url);
 }
+
+function loadJSON(filePath) {
+  // Load json file;
+  var json = loadTextFileAjaxSync(filePath, "application/json");
+  // Parse json
+  return JSON.parse(json);
+}   
+
+// Load text with Ajax synchronously: takes path to file and optional MIME type
+function loadTextFileAjaxSync(filePath, mimeType)
+{
+  var xmlhttp=new XMLHttpRequest();
+  xmlhttp.open("GET",filePath,false);
+  if (mimeType != null) {
+    if (xmlhttp.overrideMimeType) {
+      xmlhttp.overrideMimeType(mimeType);
+    }
+  }
+  xmlhttp.send();
+  if (xmlhttp.status==200)
+  {
+    return xmlhttp.responseText;
+  }
+  else {
+    // TODO Throw exception
+    return null;
+  }
+}
+
+
 
 
