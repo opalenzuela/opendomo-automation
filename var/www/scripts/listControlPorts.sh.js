49afc4cd10b7ad78403649e2bb816e61eaf0b9a1
opalenzuela <opalenzuela@gmail.com>
2014-07-04 08:50:48 +0200

    trying with getJSON

--- var/www/scripts/listControlPorts.sh.js
+++ var/www/scripts/listControlPorts.sh.js
@@ -2,13 +2,22 @@ include_script("/data/odauto.json");
 
 
 $(document).ready(function() {
-	setInterval(updatePorts, 2000);
+	updatePorts();
 });
 
-	
 function updatePorts()
 {
-
+	$.getJSON( "data/odauto.json", function( data ) {
+		var items = [];
+		$.each( data, function( key, val ) {
+			items.push( "<li id='" + key + "'>" + val + "</li>" );
+		});
+ 
+	$( "<ul/>", {
+		"class": "my-new-list",
+		html: items.join( "" )
+	}).appendTo( "body" );
+});
 }
 
 
