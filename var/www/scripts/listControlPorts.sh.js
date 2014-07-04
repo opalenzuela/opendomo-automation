4eba4c71080428c393fde4d07ad43921e277ef2e
opalenzuela <opalenzuela@gmail.com>
2014-07-04 08:54:11 +0200

    cleanup

--- var/www/scripts/listControlPorts.sh.js
+++ var/www/scripts/listControlPorts.sh.js
@@ -1,23 +1,21 @@
-include_script("/data/odauto.json");
 
-
-$(document).ready(function() {
+jQuery(function($) {
 	updatePorts();
 });
 
 function updatePorts()
 {
-	$.getJSON( "data/odauto.json", function( data ) {
+	$.getJSON( "/data/odauto.json", function( data ) {
 		var items = [];
 		$.each( data, function( key, val ) {
 			items.push( "<li id='" + key + "'>" + val + "</li>" );
 		});
  
-	$( "<ul/>", {
-		"class": "my-new-list",
-		html: items.join( "" )
-	}).appendTo( "body" );
-});
+		$( "<ul/>", {
+			"class": "my-new-list",
+			html: items.join( "" )
+		}).appendTo( "body" );
+	});
 }
 
 
