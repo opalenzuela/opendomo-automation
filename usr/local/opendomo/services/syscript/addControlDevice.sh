509e8eb7a71d9499c40a7bac8a46510a11cedaa5
opalenzuela <opalenzuela@gmail.com>
2014-08-07 12:08:55 +0200

    Fixing #4

--- usr/local/opendomo/services/syscript/addControlDevice.sh
+++ usr/local/opendomo/services/syscript/addControlDevice.sh
@@ -12,7 +12,7 @@ if test -z "$3"; then
 	# No parameters
 	echo "#> Add Control device"
 	echo "form:`basename $0`"
-	echo "	ipaddress	IP address	text	$1"
+	echo "	ipaddress	URL	text	$1"
 	echo "	username	Username	text	$2"
 	echo "	password	Password	text	$3"
 	echo "	type	Type	list[ODControl,ODControl2]	ODControl2"
@@ -25,7 +25,7 @@ else
 	TMPFILE="/var/opendomo/tmp/controlconfig.tmp"
 	
 	case "$TYPE" in
-		"ODControl")
+		ODControl|ODControl2)
 			if wget -q $URL/ver --http-user=$USER --http-password=$PASS -O $TMPFILE
 			then
 				if grep -q DONE $TMPFILE
@@ -39,14 +39,18 @@ else
 					echo "TYPE=ODControl2" >> $CFGFILE
 					echo "#INFO The device was created and it will be available after the service is restarted"
 				else
-					echo "#ERROR Invalid response from device"
+					echo "#ERR: Invalid response from device"
 				fi	
 			else
-				echo "#ERROR The device is not available at this moment or credentials were wrong"
+				echo "#ERR: The device is not available at this moment or credentials were wrong"
 			fi
+			rm $TMPFILE
+		;;
+		*)
+			echo "#ERR: Unknown device type [$TYPE]"
+			exit 1
 		;;
 	esac
 	echo
-	rm $TMPFILE
 fi
 
