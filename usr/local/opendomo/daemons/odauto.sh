901d0bcc586c185f6fd65498220fd83704c7b279
opalenzuela <opalenzuela@gmail.com>
2014-08-22 09:45:09 +0200

    Silencing and sending to background

--- usr/local/opendomo/daemons/odauto.sh
+++ usr/local/opendomo/daemons/odauto.sh
@@ -41,7 +41,7 @@ do_background() {
 			#echo -n "($DEVNAME)"
 			case "$TYPE" in
 				odcontrol|odcontrol2)
-					/bin/sh /usr/local/opendomo/bin/bind_odcontrol.sh /etc/opendomo/control/$DEVNAME.conf >/dev/null
+					/bin/sh /usr/local/opendomo/bin/bind_odcontrol.sh /etc/opendomo/control/$DEVNAME.conf >/dev/null 2>/dev/null &
 				;;
 				undefined|*)
 					logevent odauto error "Unknown device type $TYPE"
