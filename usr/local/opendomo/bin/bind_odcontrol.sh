c89f2492435d5f2ddf61dacf22d5714859d54cb1
opalenzuela <opalenzuela@gmail.com>
2014-08-22 09:39:31 +0200

    Missing some conditions

--- usr/local/opendomo/bin/bind_odcontrol.sh
+++ usr/local/opendomo/bin/bind_odcontrol.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#desc:Bind to ODControl device
+#desc:Bind to ODControl2 device
 #package:odauto
 
 ### Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
@@ -11,6 +11,9 @@ else
 	if test -f /etc/opendomo/control/$1.conf
 	then
 		source /etc/opendomo/control/$1.conf
+	else
+		echo "#ERROR: Invalid configuration file"
+		exit 1
 	fi
 fi
 
@@ -20,87 +23,110 @@ TMPFILE=/var/opendomo/tmp/$DEVNAME.tmp
 LISTFILE=/var/opendomo/tmp/$DEVNAME.lst
 CFGDIR=/etc/opendomo/control
 CTRLDIR=/var/opendomo/control
+
+# Preparations:
+test -d $CTRLDIR/$DEVNAME/ || mkdir -p $CTRLDIR/$DEVNAME/
+test -d /var/www/data || mkdir -p /var/www/data
+
+
 while test -f $PIDFILE
 do
 	# ODControl2 syntax (lsc)
-	if wget -q $URL/lsc --http-user=$USER --http-password=$PASS -O $TMPFILE 
-	then
-		#cutting columns and removing system ports
-		cut -f1,2,3 -d: $TMPFILE  | grep -v '\$' > $LISTFILE
-	else
-		# ODControl1.6 syntax (lst)
-		wget -q $URL/lst --http-user=$USER --http-password=$PASS -O $TMPFILE
-		cut -f2,3,1 -d: $TMPFILE > $LISTFILE
-	fi
+	#if wget -q $URL/lsc --http-user=$USER --http-password=$PASS -O $TMPFILE 
+	#then
+	#	#cutting columns and removing system ports
+	#	cut -f1,2,3 -d: $TMPFILE  | grep -v '\$' > $LISTFILE
+	#else
+	#	# ODControl1.6 syntax (lst) 
+	#TODO: this should be moved to another bind
+	#	wget -q $URL/lst --http-user=$USER --http-password=$PASS -O $TMPFILE
+	#	cut -f2,3,1 -d: $TMPFILE > $LISTFILE
+	#fi
 
 	#Repeated query error
-	if grep -q E003 $TMPFILE
+	#if grep -q E003 $TMPFILE
+	#then
+	
+	
+	# Avoid duplicated call error (E003)
+	wget $URL/ver --http-user=$USER --http-password=$PASS -O - > /dev/null
+	
+	# Making the actual call
+	if	wget $URL/lsc --http-user=$USER --http-password=$PASS -O $TMPFILE 
 	then
-		wget -q $URL/ver --http-user=$USER --http-password=$PASS -O - > /dev/null
-		wget -q $URL/lsc --http-user=$USER --http-password=$PASS -O $TMPFILE 
-	fi
-
-	if grep -q DONE $TMPFILE
-	then
-		test -d $CTRLDIR/$DEVNAME/ || mkdir -p $CTRLDIR/$DEVNAME/
-		echo >  /var/www/data/$DEVNAME.odauto
-		
-		# LSTFILE contiene el listado correcto
-		for line in `cat $LISTFILE | xargs` 
-		do
-			if test "$line" != "DONE"
-			then
-				PNAME=`echo $line | cut -f1 -d:`
-				PTYPE=`echo $line | cut -f2 -d:  | cut -b1-2`
-				PVAL=`echo $line | cut -f3 -d:`
-				PTAG=`echo $line | cut -f2 -d:  | cut -b4`
-				
-				INFOFILE="$CFGDIR/$DEVNAME/$PNAME.info"
-				# Only if the port was never configured, generate INFOFILE
-				if ! test -f $INFOFILE
+		if grep -q DONE $TMPFILE
+		then
+			echo "Response with DONE. Continue."
+			# Filtering and formatting output, removing system ports ($)
+			cut -f1,2,3 -d: $TMPFILE  | grep -v '\$' > $LISTFILE
+			
+			echo >  /var/www/data/$DEVNAME.odauto.tmp
+			
+			# LSTFILE contiene el listado correcto
+			for line in `cat $LISTFILE | xargs` 
+			do
+				if test "$line" != "DONE"
 				then
-					echo "Missing $INFOFILE"
-					# Obtain type
-					case "$PTYPE" in
-						DO|DV|Dv)
-							echo "way='out'" > $INFOFILE
-							# Only write customport if it does not exist
-							if ! test -f $CTRLDIR/$DEVNAME/$PNAME; then
-								echo -e "#!/bin/sh \n . $CFGDIR/$DEVNAME.conf  \n wget -q $URL/set+$PNAME+\$1 --http-user=\$USER --http-password=\$PASS -O /dev/null " > $CTRLDIR/$DEVNAME/$PNAME
-								chmod +x $CTRLDIR/$DEVNAME/$PNAME  
-							fi					
-						;;
-						DI|AI)
-							echo "way='in'" > $INFOFILE
-						;;
-						*)
-							echo "way='disabled'" > $INFOFILE
-						;;
-					esac
+					PNAME=`echo $line | cut -f1 -d:`
+					PTYPE=`echo $line | cut -f2 -d:  | cut -b1-2`
+					PVAL=`echo $line | cut -f3 -d:`
+					PTAG=`echo $line | cut -f2 -d:  | cut -b4`
 					
-					#Obtain tag
-					if test "$PTAG" != "_"
+					INFOFILE="$CFGDIR/$DEVNAME/$PNAME.info"
+					# Only if the port was never configured, generate INFOFILE
+					if ! test -f $INFOFILE
 					then
-						cd /etc/opendomo/tags/
-						TAG=`ls $PTAG*`
-					else
-						TAG=""
+						echo "Missing $INFOFILE"
+						# Obtain type
+						case "$PTYPE" in
+							DO|DV|Dv)
+								echo "way='out'" > $INFOFILE
+								# Only write customport if it does not exist
+								if ! test -f $CTRLDIR/$DEVNAME/$PNAME; then
+									echo -e "#!/bin/sh \n . $CFGDIR/$DEVNAME.conf  \n wget -q $URL/set+$PNAME+\$1 --http-user=\$USER --http-password=\$PASS -O /dev/null " > $CTRLDIR/$DEVNAME/$PNAME
+									chmod +x $CTRLDIR/$DEVNAME/$PNAME  
+								fi					
+							;;
+							DI|AI)
+								echo "way='in'" > $INFOFILE
+							;;
+							*)
+								echo "way='disabled'" > $INFOFILE
+							;;
+						esac
+						
+						#Obtain tag
+						if test "$PTAG" != "_"
+						then
+							cd /etc/opendomo/tags/
+							TAG=`ls $PTAG*`
+						else
+							TAG=""
+						fi
+						echo "tag='$TAG'" >> $INFOFILE
+						
 					fi
-					echo "tag='$TAG'" >> $INFOFILE
 					
+					# Always, refresh the port value
+					echo $PVAL  > $CTRLDIR/$DEVNAME/$PNAME.value
+					
+					# Finally, generate JSON fragment
+					echo "{\"Name\":\"$PNAME\",\"Type\":\"$PTYPE-$PTAG\",\"Tag\":\"$tag\",\"Value\":\"$PVAL\",\"Id\":\"$DEVNAME/$PNAME\"}," >> /var/www/data/$DEVNAME.odauto.tmp
 				fi
-				
-				# Always, refresh the port value
-				echo $PVAL  > $CTRLDIR/$DEVNAME/$PNAME.value
-				
-				# Finally, generate JSON fragment
-				echo "{\"Name\":\"$PNAME\",\"Type\":\"$PTYPE-$PTAG\",\"Tag\":\"$tag\",\"Value\":\"$PVAL\",\"Id\":\"$DEVNAME/$PNAME\"}," >> /var/www/data/$DEVNAME.odauto
-			fi
-		done
-	else	
-		echo "#ERR: The query ended with an error"
+			done
+		else	
+			echo "#ERR: The query ended with an error"
+			cat $TMPFILE
+		fi
+	
+	else
+		echo "#WARN: ODControl not responding. We will keep trying"
 	fi
-	# limpieza
+	
+	# A very quick replacement of the old file with the new one:
+	cat /var/www/data/$DEVNAME.odauto.tmp > /var/www/data/$DEVNAME.odauto
+	
+	# Cleanup
 	rm $TMPFILE $LISTFILE
 	sleep $REFRESH
 done
--- var/www/scripts/listControlPorts.sh.js
+++ var/www/scripts/listControlPorts.sh.js
@@ -9,46 +9,69 @@ function updatePorts()
 	var url= "/data/odauto.json";
 	var list = document.getElementById("listControlPorts");
 	portdata = loadJSON(url);
-	list.innerHTML = "";
-	for(var i=0;i<portdata.ports.length;i++) {
-		var p = portdata.ports[i];
-		if ((p.Value) && (p.Value !="") && (p.Name.indexOf("$")==-1)) {
-			switch(p.Type.substr(0,2)) {
-				case "Dv":
-				case "DV":
-				case "DO":
-					var li = document.createElement("li");
-					li.setAttribute("title",p.Id);
-					li.className="subcommand";
-					li.setAttribute("value",p.Value=="ON"?"OFF":"ON");
-					li.onclick = function() {
-						var uri = "/cgi-bin/od.cgi/listControlPorts.sh?port="+this.title+"&value="+this.getAttribute("value");
-						$.get(uri,function(){
-							setTimeout(updatePorts,1000);
-							}
-						);
-					}
-					li.innerHTML="<label>"+p.Name+ "</label><p class='DO " + p.Value.toLowerCase() + "'><a class='sw-" + p.Value.toLowerCase() + "'> </a></p>";
-					list.appendChild(li);
-					break;
-				case "DI":
-					var li = document.createElement("li");
-					li.setAttribute("title",p.Id);
-					li.className="DI";
-					li.setAttribute("value",p.Value=="ON"?"OFF":"ON");
-					li.innerHTML="<label>"+p.Name+ "</label><p class='DI " + p.Value.toLowerCase() + "'><a class='sw-" + p.Value.toLowerCase() + "'> </a></p>";
-					list.appendChild(li);				
-					break;
-				case "AI":
-					var li = document.createElement("li");
-					li.setAttribute("title",p.Id);
-					li.className="subcommand";
-					li.setAttribute("value",p.Value=="ON"?"OFF":"ON");
-					li.innerHTML="<label>"+p.Name+ "</label><p class='ro'>" + parseFloat(p.Value) + "</p>";
-					list.appendChild(li);				
-					break;
-				case "AO":
-					break;
+	if (portdata) {
+		list.innerHTML = "";
+		for(var i=0;i<portdata.ports.length;i++) {
+			var p = portdata.ports[i];
+			if ((p.Value) && (p.Value !="") && (p.Name.indexOf("$")==-1)) {
+				switch(p.Type.substr(0,2)) {
+					case "Dv":
+					case "DV":
+					case "DO":
+						var li = document.createElement("li");
+						li.setAttribute("title",p.Id);
+						li.className="subcommand";
+						li.setAttribute("value",p.Value=="ON"?"OFF":"ON");
+						li.onclick = function() {
+							var uri = "/cgi-bin/od.cgi/listControlPorts.sh?port="+this.title+"&value="+this.getAttribute("value");
+							$.get(uri,function(){
+								setTimeout(updatePorts,1000);
+								}
+							);
+						}
+						li.innerHTML="<label>"+p.Name+ "</label><p class='DO " + p.Value.toLowerCase() + "'><a class='sw-" + p.Value.toLowerCase() + "'> </a></p>";
+						list.appendChild(li);
+						break;
+					case "DI":
+						var li = document.createElement("li");
+						li.setAttribute("title",p.Id);
+						li.className="DI";
+						li.setAttribute("value",p.Value=="ON"?"OFF":"ON");
+						li.innerHTML="<label>"+p.Name+ "</label><p class='DI " + p.Value.toLowerCase() + "'><a class='sw-" + p.Value.toLowerCase() + "'> </a></p>";
+						list.appendChild(li);				
+						break;
+					case "AI":
+						var li = document.createElement("li");
+						li.setAttribute("title",p.Id);
+						li.className="subcommand";
+						li.setAttribute("value",p.Value=="ON"?"OFF":"ON");
+						li.innerHTML="<label>"+p.Name+ "</label><p class='ro'>" + parseFloat(p.Value) + "</p>";
+						list.appendChild(li);				
+						break;
+					case "AO":
+						var rng = document.createElement("input");
+						rng.setAttribute("type","range");
+						rng.value =  p.Value;
+						var li = document.createElement("li");
+						li.setAttribute("title",p.Id);
+						rng.onchange = function() {
+							var uri = "/cgi-bin/od.cgi/listControlPorts.sh?port="+this.title+"&value="+this.getAttribute("value");
+							$.get(uri,function(){
+								setTimeout(updatePorts,1000);
+								}
+							);
+						}
+						li.innerHTML="<label>"+p.Name+ "</label><p class='AO' id='"+p.Id+"_cont'></p>";
+						list.appendChild(li);
+						$("#"+p.Id+"_cont").appendChild(rng);
+						break;
+					case "TXT":
+						case "AI":
+						var li = document.createElement("li");
+						li.innerHTML="<label>"+p.Name+ "</label><p class='ro'>" + p.Value + "</p>";
+						list.appendChild(li);				
+						break;
+				}
 			}
 		}
 	}
