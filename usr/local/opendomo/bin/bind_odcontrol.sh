035706512eae5923dccb2d98e8e799c89fb372aa
root <root@OpenDomoOS.(none)>
2014-09-22 00:33:15 +0200

    Adapting

--- usr/local/opendomo/bin/bind_odcontrol.sh
+++ usr/local/opendomo/bin/bind_odcontrol.sh
@@ -75,93 +75,89 @@ do
 		wget -q $URL/content_hme_4.html --http-user=$USER --http-password=$PASS -O $TMPFILE.4 
 		
 		cat $TMPFILE.* > $TMPFILE
-			echo "Response with DONE. Continue."
-			# Filtering and formatting output, removing system ports ($)
-			cut -f1,2,3 -d: $TMPFILE  | grep -v '\$' > $LISTFILE
-			
-			echo >  /var/www/data/$DEVNAME.odauto.tmp
-			
-			# LSTFILE contiene el listado correcto
-			for line in `cat $LISTFILE | xargs` 
-			do
-				if test "$line" != "DONE"
-				then
-					PNAME=`echo $line | cut -f1 -d:`
-					PTYPE=`echo $line | cut -f2 -d:  | cut -b1-2`
-					PVAL=`echo $line | cut -f3 -d:`
-					PTAG=`echo $line | cut -f2 -d:  | cut -b4`
-					
-					INFOFILE="$CFGDIR/$DEVNAME/$PNAME.info"
-					# Only if the port was never configured, generate INFOFILE
-					if ! test -f $INFOFILE
-					then
-						echo "Missing $INFOFILE"
-						# Configuration for INPUT / OUTPUT
-						case "$PTYPE" in
-							DO|DV|Dv|AO|AV)
-								# Only write customport if it does not exist
-								if test -f $CTRLDIR/$DEVNAME/$PNAME; then
-									echo "Port $PNAME exists"
-								else
-									echo "Creating $CTRLDIR/$DEVNAME/$PNAME"
-									echo -e "#!/bin/sh \n . $CFGDIR/$DEVNAME.conf  \n wget -q $URL:81/set+$PNAME+\$1 --http-user=\$USER --http-password=\$PASS -O /dev/null " > $CTRLDIR/$DEVNAME/$PNAME
-									chmod +x $CTRLDIR/$DEVNAME/$PNAME  
-								fi					
-								# Saving info
-								echo "way='out'" > $INFOFILE
-
-							;;
-							DI|AI)
-								echo "way='in'" > $INFOFILE
-							;;
-							*)
-								echo "way='disabled'" > $INFOFILE
-							;;
-						esac
-							
-						# Special configuration for ANALOG / DIGITAL
-						case "$PTYPE" in 
-							AO|AV|AI)
-								MIN=`echo $line | cut -f4 -d: | cut -f1 -d'|'`
-								MAX=`echo $line | cut -f4 -d: | cut -f2 -d'|'` 
-								echo "min='$MIN'" >> $INFOFILE
-								echo "max='$MAX'" >> $INFOFILE							
-							;;
-						esac
-						
-						
-						# Generic configurations: Obtain tag
-						if test "$PTAG" != "_"
-						then
-							cd /etc/opendomo/tags/
-							TAG=`ls $PTAG*`
-						else
-							TAG=""
-						fi
-						echo "tag='$TAG'" >> $INFOFILE
-					fi
-					
-					# These values shall be override in $INFOFILE
-					desc=$PNAME
-					min=0
-					max=100
-					source $INFOFILE
-					
-					# Always, refresh the port value
-					echo $PVAL  > $CTRLDIR/$DEVNAME/$PNAME.value
-					
-					# Finally, generate JSON fragment
-					if test "$status" != "disabled"
-					then
-						echo "{\"Name\":\"$desc\",\"Type\":\"$PTYPE\",\"Tag\":\"$tag\",\"Value\":\"$PVAL\",\"Min\":\"$min\",\"Max\":\"$max\",\"Id\":\"$DEVNAME/$PNAME\"}," >> /var/www/data/$DEVNAME.odauto.tmp
-					fi
-				fi
-			done
-		else	
-			echo "#ERR: The query ended with an error"
-			cat $TMPFILE
-		fi
+		echo "Response with DONE. Continue."
 	
+		# Filtering and formatting output, removing system ports ($)
+		cut -f1,2,3 -d: $TMPFILE  | grep -v '\$' > $LISTFILE
+			
+		echo >  /var/www/data/$DEVNAME.odauto.tmp
+			
+		# LSTFILE contiene el listado correcto
+		for line in `cat $LISTFILE | xargs` 
+		do
+			if test "$line" != "DONE"
+			then
+				PNAME=`echo $line | cut -f1 -d:`
+				PTYPE=`echo $line | cut -f2 -d:  | cut -b1-2`
+				PVAL=`echo $line | cut -f3 -d:`
+				PTAG=`echo $line | cut -f2 -d:  | cut -b4`
+					
+				INFOFILE="$CFGDIR/$DEVNAME/$PNAME.info"
+				# Only if the port was never configured, generate INFOFILE
+				if ! test -f $INFOFILE
+				then
+					echo "Missing $INFOFILE"
+					# Configuration for INPUT / OUTPUT
+					case "$PTYPE" in
+						DO|DV|Dv|AO|AV)
+							# Only write customport if it does not exist
+							if test -f $CTRLDIR/$DEVNAME/$PNAME; then
+								echo "Port $PNAME exists"
+							else
+								echo "Creating $CTRLDIR/$DEVNAME/$PNAME"
+								echo -e "#!/bin/sh \n . $CFGDIR/$DEVNAME.conf  \n wget -q $URL:81/set+$PNAME+\$1 --http-user=\$USER --http-password=\$PASS -O /dev/null " > $CTRLDIR/$DEVNAME/$PNAME
+								chmod +x $CTRLDIR/$DEVNAME/$PNAME  
+							fi					
+							# Saving info
+							echo "way='out'" > $INFOFILE
+
+						;;
+						DI|AI)
+							echo "way='in'" > $INFOFILE
+						;;
+						*)
+							echo "way='disabled'" > $INFOFILE
+						;;
+					esac
+							
+					# Special configuration for ANALOG / DIGITAL
+					case "$PTYPE" in 
+						AO|AV|AI)
+							MIN=`echo $line | cut -f4 -d: | cut -f1 -d'|'`
+							MAX=`echo $line | cut -f4 -d: | cut -f2 -d'|'` 
+							echo "min='$MIN'" >> $INFOFILE
+							echo "max='$MAX'" >> $INFOFILE							
+						;;
+					esac
+						
+					
+					# Generic configurations: Obtain tag
+					if test "$PTAG" != "_"
+					then
+						cd /etc/opendomo/tags/
+						TAG=`ls $PTAG*`
+					else
+						TAG=""
+					fi
+					echo "tag='$TAG'" >> $INFOFILE
+				fi
+					
+				# These values shall be override in $INFOFILE
+				desc=$PNAME
+				min=0
+				max=100
+				source $INFOFILE
+					
+				# Always, refresh the port value
+				echo $PVAL  > $CTRLDIR/$DEVNAME/$PNAME.value
+					
+				# Finally, generate JSON fragment
+				if test "$status" != "disabled"
+				then
+					echo "{\"Name\":\"$desc\",\"Type\":\"$PTYPE\",\"Tag\":\"$tag\",\"Value\":\"$PVAL\",\"Min\":\"$min\",\"Max\":\"$max\",\"Id\":\"$DEVNAME/$PNAME\"}," >> /var/www/data/$DEVNAME.odauto.tmp
+				fi
+			fi
+		done
 	else
 		echo "#WARN: ODControl not responding. We will keep trying"
 	fi
