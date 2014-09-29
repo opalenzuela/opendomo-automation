f417359ef6c1180b35f05a3bbce921677260bb67
opalenzuela <opalenzuela@gmail.com>
2014-09-29 12:39:41 +0200

    Fixing boolean parameter

--- var/www/scripts/addControlDevice.sh.js
+++ var/www/scripts/addControlDevice.sh.js
@@ -2,18 +2,18 @@ $(function(){
 	$("#type").on("change",function() {
 		switch($(this).val()){
 			case "odcontrol2":
-				$("#username").val("user").prop("disabled","true");
+				$("#username").val("user").prop("disabled",true);
 				$("#password_li, #username_li").show();
 				break;
 			case "odcontrol":
-				$("#username").val("admin").prop("disabled","true");
+				$("#username").val("admin").prop("disabled",true);
 				$("#password_li, #username_li").show();
 				break;
 			case "domino":
 				$("#password_li, #username_li").hide();
 				break;
 			default:
-				$("#username").val("").prop("disabled","false");
+				$("#username").prop("disabled",false);
 				$("#password_li, #username_li").show();
 			
 		};
