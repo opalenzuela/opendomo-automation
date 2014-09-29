fa61d7ca37d33f78731bfbcc3c435c431d0fd3db
opalenzuela <opalenzuela@gmail.com>
2014-09-29 12:37:20 +0200

    Adding JS file

---
+++ var/www/scripts/addControlDevice.sh.js
@@ -0,0 +1,21 @@
+$(function(){
+	$("#type").on("change",function() {
+		switch($(this).val()){
+			case "odcontrol2":
+				$("#username").val("user").prop("disabled","true");
+				$("#password_li, #username_li").show();
+				break;
+			case "odcontrol":
+				$("#username").val("admin").prop("disabled","true");
+				$("#password_li, #username_li").show();
+				break;
+			case "domino":
+				$("#password_li, #username_li").hide();
+				break;
+			default:
+				$("#username").val("").prop("disabled","false");
+				$("#password_li, #username_li").show();
+			
+		};
+	});
+});
