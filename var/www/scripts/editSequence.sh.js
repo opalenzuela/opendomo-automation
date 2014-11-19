2d64cd548d39a784ac708002ca54937cc4e5fc20
opalenzuela <opalenzuela@gmail.com>
2014-11-19 12:44:26 +0100

    Trying to delete by dragging as well

--- var/www/scripts/editSequence.sh.js
+++ var/www/scripts/editSequence.sh.js
@@ -3,10 +3,16 @@ include_script("/scripts/vendor/jquery-ui.js");
 $(function($) {
 	setTimeout(sequenceDragandropEnable,1000);
 });
-
+var sortableIn = 0;
 function sequenceDragandropEnable(){
 	$( "#editSequence" ).sortable({
-      revert: true
+		revert: true,
+		receive: function(event, ui){sortableIn = 1;},
+		over: function(event, ui){sortableIn = 1;},
+		out: function(event, ui){sortableIn = 0;},
+		beforeStop: function(event, ui){
+			if (sortableIn == 0) ui.item.remove();
+		}
     });
     $( "#editSequenceSteps li.item" ).draggable({
 		connectToSortable: "#editSequence",
