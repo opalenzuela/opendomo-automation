c275728de855dbea91929e551d6bb40f961e7546
opalenzuela <opalenzuela@gmail.com>
2014-11-25 10:41:50 +0100

    Full conditions

--- var/www/scripts/manageRules.sh.js
+++ var/www/scripts/manageRules.sh.js
@@ -3,7 +3,7 @@ include_script("/scripts/vendor/jquery-ui.js");
 $(function($) {
 	setTimeout(ruleDragandropEnable,100);
 	$('button[name="submit_manageRules.sh"]').on("click",saverule);
-	$("body").append("<div id='dialog' title='Enter a value'><p></p></div>");
+	$("body").append("<div id='dialog' title='Enter a value'><p class='dialogcomp'></p><p class='dialogval'></p></div>");
 });
 var sortableIn = 0;
 function ruleDragandropEnable(){
@@ -19,26 +19,20 @@ function ruleDragandropEnable(){
 				ui.item.remove();
 			} else {
 				var command = $(ui.item).find("input").val();
-
-				if ((command.indexOf("???")>0) ||((command.indexOf("[")>0) && (command.indexOf("]")>0))){
-					if (command.indexOf("???")>0){
-						var possible = command.split(/\?\?\?/);
-						$("#dialog p").html("<input id='dialogvalue' type='text' />");
-					} else {
-						var possible = command.split(/[\[\]]/);
-					}
-					
+				$("p.dialogcomp").html("<select id='dialogcomparison'><option value='-lt'>Smaller</option><option value='='>Equal</option><option value='-gt'>Greater</option></select>");
+				
+				if ((command.indexOf("[")>0) && (command.indexOf("]")>0)){
 					if (possible[1].indexOf(",")>0) {
 						var def = possible[1].split(",");
 						for (var i=0;i<def.length;i++) {
 							htmlcode=htmlcode+"<option value='"+def[i]+"'>"+def[i]+"</option>";
 						}
-						$("#dialog p").html("<select id='dialogvalue'> " + htmlcode + "</select>");
+						$("p.dialogval").html("<select id='dialogvalue'> " + htmlcode + "</select>");
 					} 
 					
 					if (possible[1].indexOf("-")>0) {
 						var def = possible[1].split("-");
-						$("#dialog p").html("<input id='dialogvalue' type='range' min='" + def[0] +"'  max='"+ def[1] + "'><br/><div id='dialogvaluerender'></div>");
+						$("p.dialogval").html("<input id='dialogvalue' type='range' min='" + def[0] +"'  max='"+ def[1] + "'><br/><div id='dialogvaluerender'></div>");
 						$("#dialogvalue").on("change", function(){$("#dialogvaluerender").text($(this).val())});
 					}
 					//command = possible[0] +  prompt("Choose value between " + possible[1], def[0]) + possible[2];
@@ -50,7 +44,8 @@ function ruleDragandropEnable(){
 						buttons: {
 							"Ok": function() {
 								var value = $("#dialogvalue").val();
-								command = possible[0] + value + possible[2];
+								var comparison = $("#dialogcomparison").val();
+								command = possible[0] + comparison + value;
 								$(ui.item).find("input").val(command);								
 								$(ui.item).find("p").text($(ui.item).find("p").text().replace("???",value));
 								$( this ).dialog( "close" );
