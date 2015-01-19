3c71f3f283e342c207d03218414a3b82d69ac254
opalenzuela <opalenzuela@gmail.com>
2015-01-19 17:01:25 +0100

    Delete rule

--- usr/local/opendomo/services/syscript/delRule.sh
+++ usr/local/opendomo/services/syscript/delRule.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 #desc:Delete rule
 #type:local
-#package:odai_rules
+#package:odauto
+
+# Copyright(c) 2015 OpenDomo Services SL. Licensed under GPL v3 or later
 
 RULESDIR="/etc/opendomo/rules"
-
 
 if test -z "$1"; then
 	echo "#INFO: No rule specified"
 	exit 1
 fi
 
-
 for file in "$@"
 do
-	if test -f "$RULESDIR/$file"; then
-		if rm -f "$RULESDIR/$file"; then
-			echo "#INFO: Rule deleted [$file]"
+	if test -f "$RULESDIR/$file.rule"; then
+		if rm -f "$RULESDIR/$file.rule"; then
+			echo "#INFO Rule deleted [$file]"
 		else
 			echo "#ERR: Cannot delete"
 			exit 2
