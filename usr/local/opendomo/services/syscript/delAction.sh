ddcda4974dc784248e0b6fd50242acad5d51e59c
opalenzuela <opalenzuela@gmail.com>
2015-02-03 11:40:29 +0100

    Deleting action (without extension)

--- usr/local/opendomo/services/syscript/delAction.sh
+++ usr/local/opendomo/services/syscript/delAction.sh
@@ -3,12 +3,12 @@
 #type:local
 #package:odauto
 
-# Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
+# Copyright(c) 2015 OpenDomo Services SL. Licensed under GPL v3 or later
 
 ACTPATH="/etc/opendomo/actions"
 
-if test -f "$ACTPATH/$1"; then
-	rm -fr "$ACTPATH/$1"
+if test -f "$ACTPATH/$1.action"; then
+	rm -fr "$ACTPATH/$1.action"
 fi
 
 # Once deleted, back to the main script
