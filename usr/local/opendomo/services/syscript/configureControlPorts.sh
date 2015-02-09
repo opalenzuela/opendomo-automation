4df62ec110bcfe895d1a6b720f2447c54280de68
opalenzuela <opalenzuela@gmail.com>
2015-02-09 12:51:56 +0100

    EOL corrected

--- usr/local/opendomo/services/syscript/configureControlPorts.sh
+++ usr/local/opendomo/services/syscript/configureControlPorts.sh
@@ -3,7 +3,7 @@
 #package:odauto
 #type:local
  
-# Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
+# Copyright(c) 2015 OpenDomo Services SL. Licensed under GPL v3 or later
 
 CTRLPATH="/var/opendomo/control"
 CFGPATH="/etc/opendomo/control"
